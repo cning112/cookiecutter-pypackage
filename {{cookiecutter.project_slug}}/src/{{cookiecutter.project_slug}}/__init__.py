4c9f3df36f3d74b764a39fb17662a113d80e2f77
Chao Ning <chao.ning.uk@gmail.com>
2024-08-05 23:49:01 +0100

    add custom logging using structlogs

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -3,3 +3,7 @@
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
+
+from .custom_logging import setup_logging
+
+setup_logging()
